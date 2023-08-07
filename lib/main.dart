aa9f69f82dfd74f343e7f218b811c166f1ab46cf
daniilamark <daniilamark@gmail.com>
2023-08-07 15:48:47 +0500

    update first

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tutorial/pages/home_page.dart';
+import 'package:tutorial/pages/login_page.dart';
+import 'package:tutorial/styles/app_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,41 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 8, 118, 207),
-        body: Column(
-          children: [
-            const Text('Hello Worlssssssssssssd!'),
-            const Text('Hello World!'),
-            const TextField(
-              decoration: InputDecoration(hintText: 'Username'),
-            ),
-            const TextField(
-              decoration: InputDecoration(hintText: 'Password'),
-            ),
-            TextButton(
-                onPressed: () {
-                  print("Forgot clicked");
-                },
-                child: const Text("Forgot password?")),
-            ElevatedButton(
-                onPressed: () {
-                  print("Log in clicked");
-                },
-                child: const Text("Log in")),
-            const Text('Or sign in with'),
-            ElevatedButton(
-                onPressed: () {
-                  print("Log in GOOGLE");
-                },
-                child: const Text("Login with")),
-          ],
-        ),
+      theme: ThemeData(
+        fontFamily: 'Urbanist',
+        scaffoldBackgroundColor: AppColors.background,
       ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+      },
     );
-  }
-
-  mu() {
-    print("click");
   }
 }
